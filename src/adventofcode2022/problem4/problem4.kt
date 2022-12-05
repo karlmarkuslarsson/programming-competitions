cb849271d997532c2eb3722fb6da9258223cf61c
Markus Larsson <karl.markus.larsson@gmail.com>
2022-12-05 08:15:38 +0100

    added day 4 solution

--- src/adventofcode2022/problem4/problem4.kt
+++ src/adventofcode2022/problem4/problem4.kt
@@ -6,9 +6,38 @@ fun main() {
 }
 
 fun solveA() {
-
+    var sum = 0
+    input4.split("\n").forEach { row ->
+        val rowSplit = row.split(",")
+        val firstSection = parseSection(rowSplit[0])
+        val secondSection = parseSection(rowSplit[1])
+        if (firstSection.from <= secondSection.from && firstSection.to >= secondSection.to) {
+            sum++
+        }else if (secondSection.from <= firstSection.from && secondSection.to >= firstSection.to) {
+            sum++
+        }
+    }
+    println(sum)
 }
 
-fun solveB() {
+fun parseSection(input: String): Section {
+    val sectionSplit = input.split("-")
+    return Section(sectionSplit[0].toInt(), sectionSplit[1].toInt())
+}
 
+data class Section(val from: Int, val to: Int)
+
+fun solveB() {
+    var sum = 0
+    input4.split("\n").forEach { row ->
+        val rowSplit = row.split(",")
+        val firstSection = parseSection(rowSplit[0])
+        val secondSection = parseSection(rowSplit[1])
+        if (secondSection.from <= firstSection.to && secondSection.to >= firstSection.to) {
+            sum++
+        } else if (firstSection.from <= secondSection.to && firstSection.to >= secondSection.to) {
+            sum++
+        }
+    }
+    println(sum)
 }
