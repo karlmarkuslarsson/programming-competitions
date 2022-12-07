94b4b2ae16bfffbf70a3b94eb0957039be4de9cd
Markus Larsson <karl.markus.larsson@gmail.com>
2022-12-07 07:02:17 +0100

    added day 6 solution

--- src/adventofcode2022/problem6/problem6.kt
+++ src/adventofcode2022/problem6/problem6.kt
@@ -1,14 +1,48 @@
 package adventofcode2022.problem6
 
+import java.util.LinkedList
+
 fun main() {
     solveA()
     solveB()
 }
 
 fun solveA() {
-
+    val lastCharacters = LinkedList<Char>()
+    var counter = 0
+    input6.forEach {
+        counter++
+        lastCharacters.addFirst(it)
+        if (lastCharacters.size > 4) {
+            lastCharacters.removeLast()
+        }
+        if (lastCharacters.size == 4) {
+            val set = hashSetOf<Char>()
+            set.addAll(lastCharacters)
+            if (set.size == lastCharacters.size) {
+                println(counter)
+                return
+            }
+        }
+    }
 }
 
 fun solveB() {
-
+    val lastCharacters = LinkedList<Char>()
+    var counter = 0
+    input6.forEach {
+        counter++
+        lastCharacters.addFirst(it)
+        if (lastCharacters.size > 14) {
+            lastCharacters.removeLast()
+        }
+        if (lastCharacters.size == 14) {
+            val set = hashSetOf<Char>()
+            set.addAll(lastCharacters)
+            if (set.size == lastCharacters.size) {
+                println(counter)
+                return
+            }
+        }
+    }
 }
