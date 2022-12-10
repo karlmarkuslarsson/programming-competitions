b4d85172baa7fdc8e058fcff676bfdb9a052c362
Markus Larsson <karl.markus.larsson@gmail.com>
2022-12-10 20:37:04 +0100

    added day 10 solution

--- src/adventofcode2022/problem10/problem10.kt
+++ src/adventofcode2022/problem10/problem10.kt
@@ -6,9 +6,72 @@ fun main() {
 }
 
 fun solveA() {
+    var clockCycle = 0
+    var registerValue = 1
+    var totalSum = 0
+    input10.split("\n").forEach {
+        val split = it.split(" ")
+        when (split[0]) {
+            "addx" -> {
+                for (i in 0 until 2) {
+                    clockCycle++
+                    if ((clockCycle - 20) % 40 == 0 || clockCycle == 20) {
+                        totalSum += clockCycle * registerValue
+                    }
+                }
+                registerValue += split[1].toInt()
+            }
+
+            "noop" -> {
+                for (i in 0 until 1) {
+                    clockCycle++
+                    if ((clockCycle - 20) % 40 == 0 || clockCycle == 20) {
+                        totalSum += clockCycle * registerValue
+                    }
+                }
+            }
+        }
+    }
+    println(totalSum)
 
 }
 
 fun solveB() {
+    var clockCycle = 0
+    var startX = 1
 
+    input10.split("\n").forEach {
+        val split = it.split(" ")
+        when (split[0]) {
+            "addx" -> {
+                for (i in 0 until 2) {
+                    if ((clockCycle %40) >= startX-1 && (clockCycle %40) <= startX + 1) {
+                        print("#")
+                    } else {
+                        print(".")
+                    }
+
+                    clockCycle++
+                    if (clockCycle % 40 == 0) {
+                        println()
+                    }
+                }
+                startX += split[1].toInt()
+            }
+
+            "noop" -> {
+                for (i in 0 until 1) {
+                    if ((clockCycle % 40) >= startX-1 && (clockCycle % 40) <= startX + 1) {
+                        print("#")
+                    } else {
+                        print(".")
+                    }
+                    clockCycle++
+                    if (clockCycle % 40 == 0) {
+                        println()
+                    }
+                }
+            }
+        }
+    }
 }
